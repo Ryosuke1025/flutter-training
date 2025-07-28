a79c19bdace04360bd27a30baa9d747ef2d9704e
Ryosuke Suzaki <r_suzaki@yumemi.co.jp>
2025-07-28 21:54:17 +0900

    Add WeatherConditionWidget and WeatherConditionUiExtension for weather icon display

---
+++ lib/ui/weather_condition_widget.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_training/services/entity/weather_condition.dart';
+import 'package:flutter_training/ui/extension/weather_condition_ui_extension.dart';
+
+class WeatherConditionWidget extends StatelessWidget {
+  const WeatherConditionWidget({
+    required this.weatherCondition,
+    super.key,
+  });
+
+  final WeatherCondition? weatherCondition;
+
+  @override
+  Widget build(BuildContext context) {
+    return AspectRatio(
+      aspectRatio: 1,
+      child: weatherCondition?.icon ?? const Placeholder(),
+    );
+  }
+
+  @override
+  void debugFillProperties(DiagnosticPropertiesBuilder properties) {
+    super.debugFillProperties(properties);
+    properties.add(
+      EnumProperty<WeatherCondition>('weatherCondition', weatherCondition),
+    );
+  }
+}
