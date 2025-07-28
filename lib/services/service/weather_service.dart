7a2f42f0ad8d6c53919208c6a246439d23bdc366
Ryosuke Suzaki <r_suzaki@yumemi.co.jp>
2025-07-28 18:11:07 +0900

    Add WeatherService to fetch weather conditions using YumemiWeather API

---
+++ lib/services/service/weather_service.dart
@@ -0,0 +1,13 @@
+import 'package:flutter_training/services/entity/weather_condition.dart';
+import 'package:yumemi_weather/yumemi_weather.dart';
+
+class WeatherService {
+  final _weather = YumemiWeather();
+
+  WeatherCondition fetchWeather() {
+    final weatherConditionString = _weather.fetchSimpleWeather();
+    return WeatherCondition.values.firstWhere(
+      (e) => e.name == weatherConditionString,
+    );
+  }
+}
