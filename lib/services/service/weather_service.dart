9a615daa27331e04a1d81703a55bd6903a744914
Ryosuke Suzaki <r_suzaki@yumemi.co.jp>
2025-07-28 22:05:10 +0900

    Update collection dependency to direct main and modify fetchWeather method to use firstWhereOrNull

--- lib/services/service/weather_service.dart
+++ lib/services/service/weather_service.dart
@@ -1,12 +1,13 @@
+import 'package:collection/collection.dart';
 import 'package:flutter_training/services/entity/weather_condition.dart';
 import 'package:yumemi_weather/yumemi_weather.dart';
 
 class WeatherService {
   final _weather = YumemiWeather();
 
-  WeatherCondition fetchWeather() {
+  WeatherCondition? fetchWeather() {
     final weatherConditionString = _weather.fetchSimpleWeather();
-    return WeatherCondition.values.firstWhere(
+    return WeatherCondition.values.firstWhereOrNull(
       (e) => e.name == weatherConditionString,
     );
   }
