10e620152db295c2825369419fca4f9ea5bb3443
Ryosuke Suzaki <r_suzaki@yumemi.co.jp>
2025-08-05 11:32:54 +0900

    Update pubspec.yaml to include flutter_gen configuration and add build_runner dependencies; refactor asset paths in WeatherConditionUiExtension to use generated assets.

--- lib/ui/extension/weather_condition_ui_extension.dart
+++ lib/ui/extension/weather_condition_ui_extension.dart
@@ -1,23 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:flutter_training/services/entity/weather_condition.dart';
+import 'package:flutter_training/gen/assets.gen.dart';
 
 extension WeatherConditionUiExtension on WeatherCondition {
   Widget get icon {
     switch (this) {
       case WeatherCondition.sunny:
         return SvgPicture.asset(
-          'assets/sunny.svg',
+          Assets.sunny,
           semanticsLabel: 'sunny',
         );
       case WeatherCondition.cloudy:
         return SvgPicture.asset(
-          'assets/cloudy.svg',
+          Assets.cloudy,
           semanticsLabel: 'cloudy',
         );
       case WeatherCondition.rainy:
         return SvgPicture.asset(
-          'assets/rainy.svg',
+          Assets.rainy,
           semanticsLabel: 'rainy',
         );
     }
