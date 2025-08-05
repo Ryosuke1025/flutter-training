b0f5fe6162301b96a0db884291ea718a4e197563
Ryosuke Suzaki <r_suzaki@yumemi.co.jp>
2025-08-05 12:13:31 +0900

    Fix import order in WeatherConditionUiExtension to ensure proper asset usage

--- lib/ui/extension/weather_condition_ui_extension.dart
+++ lib/ui/extension/weather_condition_ui_extension.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:flutter_training/services/entity/weather_condition.dart';
 import 'package:flutter_training/gen/assets.gen.dart';
+import 'package:flutter_training/services/entity/weather_condition.dart';
 
 extension WeatherConditionUiExtension on WeatherCondition {
   Widget get icon {
