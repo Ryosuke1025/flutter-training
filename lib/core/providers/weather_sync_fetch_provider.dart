df9fd4925cbf1784a082399ffbf843631f9a1af0
Ryosuke Suzaki <r_suzaki@yumemi.co.jp>
2025-09-30 17:09:08 +0900

    Add weatherSyncFetchProvider for asynchronous weather fetching using Isolate; update MainApp and related imports

--- lib/core/providers/weather_sync_fetch_provider.dart
+++ lib/core/providers/weather_sync_fetch_provider.dart
@@ -1,10 +1,12 @@
 import 'dart:isolate';
 
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:yumemi_weather/yumemi_weather.dart';
+import 'package:flutter_training/core/providers/yumemi_weather_client_provider.dart';
 
 typedef WeatherFetchEntry = Future<String> Function(String json);
 
 final weatherSyncFetchProvider = Provider<WeatherFetchEntry>((ref) {
-  return (json) => Isolate.run(() => YumemiWeather().syncFetchWeather(json));
+  return (json) => Isolate.run(
+    () => ref.read(yumemiWeatherClientProvider).syncFetchWeather(json),
+  );
 });
